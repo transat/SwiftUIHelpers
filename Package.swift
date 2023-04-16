191d7505ea96711dbc85dfa4ef4a40520d98f0b0
Clem <clement.girault@icloud.com>
2023-04-16 16:21:16 +1000

    FIrst

--- Package.swift
+++ Package.swift
@@ -13,16 +13,27 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-custom-dump", .upToNextMajor(from: "0.3.0")),
+        .package(url: "https://github.com/pointfreeco/swift-identified-collections", .upToNextMajor(from: "0.7.1")),
+        .package(url: "https://github.com/nalexn/EnvironmentOverrides", .upToNextMajor(from: "0.0.4")),
+        .package(url: "https://github.com/inamiy/SherlockForms", .upToNextMajor(from: "0.3.0")),
+        .package(url: "https://github.com/yannickl/DynamicColor.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/SvenTiigi/WhatsNewKit.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/JohnSundell/Files.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/AppPear/ChartView", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", .upToNextMajor(from: "0.1.4")),
+        .package(url: "https://github.com/pointfreeco/swift-gen.git", .upToNextMajor(from: "0.4.0")),
+        .package(url: "https://github.com/aheze/Setting.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/divadretlaw/Settings.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/aheze/SwipeActions.git", .upToNextMajor(from: "1.0.0")),
+        
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIHelpers",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftUIHelpersTests",
-            dependencies: ["SwiftUIHelpers"]),
+            dependencies: ["CustomDump", "DynamicColor", "EnvironmentOverrides", "Files", "Settings", "Setting", "SherlockForms", "WhatsNewKit", "SwiftUICharts", "SwiftUIX", "SwipeActions", "IdentifiedCollections", "Gen"]),
     ]
 )
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SwiftUIHelpers
 
-A description of this package.
+A collection of useful packages for SwiftUI app development.
