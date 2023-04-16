f8cf816ae9dd7b9c858c8e9a1346b1b70a989165
Clem <clement.girault@icloud.com>
2023-04-16 22:16:09 +1000

    Fixed package names
    Fixed Package names

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIHelpers",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v15),
+        .macCatalyst(.v15)
+
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,11 +22,9 @@ let package = Package(
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/pointfreeco/swift-identified-collections", .upToNextMajor(from: "0.7.1")),
         .package(url: "https://github.com/nalexn/EnvironmentOverrides", .upToNextMajor(from: "0.0.4")),
-        .package(url: "https://github.com/inamiy/SherlockForms", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/yannickl/DynamicColor.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/SvenTiigi/WhatsNewKit.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/JohnSundell/Files.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/AppPear/ChartView", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", .upToNextMajor(from: "0.1.4")),
         .package(url: "https://github.com/pointfreeco/swift-gen.git", .upToNextMajor(from: "0.4.0")),
         .package(url: "https://github.com/aheze/Setting.git", .upToNextMajor(from: "1.0.0")),
@@ -33,7 +37,20 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftUIHelpers",
-            dependencies: ["swift-custom-dump", "DynamicColor", "EnvironmentOverrides", "Files", "Settings", "Setting", "SherlockForms", "WhatsNewKit", "SwiftUICharts", "SwiftUIX", "SwipeActions", "swift-identified-collections", "swift-gen"]),
+            dependencies: [
+                .product(name: "DynamicColor", package: "DynamicColor"),
+                .product(name: "Files", package: "Files"),
+                .product(name: "SwiftUIX", package: "SwiftUIX"),
+                .product(name: "CustomDump", package: "swift-custom-dump"),
+                .product(name: "Gen", package: "swift-gen"),
+                .product(name: "WhatsNewKit", package: "WhatsNewKit"),
+                .product(name: "Settings", package: "Settings"),
+                .product(name: "Setting", package: "Setting"),
+                .product(name: "IdentifiedCollections", package: "swift-identified-collections"),
+                .product(name: "SwipeActions", package: "SwipeActions"),
+                .product(name: "EnvironmentOverrides", package: "EnvironmentOverrides"),
+                ]
+        )
     ]
 )
 
--- Sources/SwiftUIHelpers/SwiftUIHelpers.swift
+++ Sources/SwiftUIHelpers/SwiftUIHelpers.swift
@@ -1,3 +1,14 @@
+@_exported import DynamicColor
+@_exported import EnvironmentOverrides
+@_exported import Files
+@_exported import Settings
+@_exported import Setting
+@_exported import CustomDump
+@_exported import Gen
+@_exported import SwiftUIX
+@_exported import SwipeActions
+@_exported import WhatsNewKit
+
 public struct SwiftUIHelpers {
     public private(set) var text = "Hello, World!"
 
